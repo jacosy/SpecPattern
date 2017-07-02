0ca7a2344e151423c4669dcc9455a012c9154456
Longoria.You <longoria.you@gmail.com>
2017-07-02 10:50:16 +0800

    Add project files.

---
+++ src/UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Logic.Utils;
+
+namespace UI
+{
+    public partial class App
+    {
+        public App()
+        {
+            Initer.Init(@"Server=(LocalDb)\MSSQLLocalDB;Database=SpecPattern;Trusted_Connection=true;");
+        }
+    }
+}
